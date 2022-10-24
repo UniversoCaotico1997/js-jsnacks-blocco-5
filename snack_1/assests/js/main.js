4c41d1d0ce51f17b7bafccf4d1a6c37678c65347
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-24 16:38:39 +0200

    Stampo lista oggetti + filter

---
+++ snack_1/assests/js/main.js
@@ -0,0 +1,60 @@
+// Crea un array di 10 oggetti che rappresentano una zucchina, indicando
+// per ognuna varietà, peso e lunghezza.
+// Calcola quanto pesano tutte le zucchine.
+
+const catalogoZucchine = [
+    {
+        varietà:`zucchinaNera`,
+        peso:20,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaBianca`,
+        peso:15,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaGialla`,
+        peso:25,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaVerde`,
+        peso:30,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaRossa`,
+        peso:17,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaArancione`,
+        peso:29,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaBlu`,
+        peso:20,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaNera`,
+        peso:22,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaViola`,
+        peso:12,
+        lunghezza: 30
+    },
+    {
+        varietà:`zucchinaSmeraldo`,
+        peso:33,
+        lunghezza: 30
+    },
+]
+
+const totalePeso = catalogoZucchine.filter((zucchina)=>{
+    zucchina.peso
+})
