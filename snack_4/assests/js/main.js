7a46dc82097de5dc862a21324750b04c5f3db031
UniversoCaotico1997 <loria.roberto97@gmail.com>
2022-10-25 09:27:53 +0200

    finish jsnack 4

---
+++ snack_4/assests/js/main.js
@@ -0,0 +1,18 @@
+// Scrivi una funzione che fonda due array (aventi lo stesso numero di
+//     elementi) prendendo alternativamente gli elementi da uno e dall’altro
+//     es. [a,b,c], [1,2,3] → [a,1,b,2,c,3].
+
+const numeri = [1,2,3];
+const lettere = ['a','b','c'];
+
+function unioneArray(primoArray, secondoArray) {
+      let numeriELettere =[];
+      primoArray.forEach(index => {
+            numeriELettere.push(primoArray[index-1]);
+            numeriELettere.push(secondoArray[index-1]);
+      });
+      return numeriELettere;
+}
+
+let numeriELettere = unioneArray(numeri, lettere);
+console.log(numeriELettere);
